44c18065e574f6f3abf1eb53cb7a72d6067b17f4
KiattipoomR <kiattipoom.r@gmail.com>
2020-12-09 09:27:28 +0700

    Added Code for solving Day 5

---
+++ Day 5/day5_1.py
@@ -0,0 +1,29 @@
+entries = []
+seat_ids = set()
+
+with open('day5.txt') as f :
+    for line in f :
+        entries.append(line)
+
+for entry in entries :
+    row_range = [0, 127]
+    col_range = [0, 7]
+
+    for letter in entry :
+        row_half = (row_range[1] - row_range[0] + 1) // 2
+        col_half = (col_range[1] - col_range[0] + 1) // 2
+
+        if letter == 'F' :
+            row_range[1] -= row_half
+        elif letter == 'B' :
+            row_range[0] += row_half
+        elif letter == 'L' :
+            col_range[1] -= col_half
+        elif letter == 'R' :
+            col_range[0] += col_half
+
+    seat_id = (row_range[0] * 8) + col_range[0]
+
+    seat_ids.add(seat_id)
+
+print(max(seat_ids))
